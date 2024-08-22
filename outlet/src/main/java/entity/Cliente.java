abadc2ce312236320200a222efc3c922e9bea75d
unknown <nicholasnloiola@hotmail.com>
2024-08-21 21:09:56 -0300

    Criação de entidades

---
+++ outlet/src/main/java/entity/Cliente.java
@@ -0,0 +1,14 @@
+package entity;
+
+import lombok.Data;
+import java.util.List;
+import jakarta.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Entity
+@Table
+public class Cliente {
+
+}
