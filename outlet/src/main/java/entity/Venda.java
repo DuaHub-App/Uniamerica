29e53847bb2dc7928991d9d75909b26b5b0bffd6
nicholasnln <nicholasnloiola@hotmail.com>
2024-08-25 03:50:53 -0300

    Adicionando e atualizando Venda e produto

--- outlet/src/main/java/entity/Venda.java
+++ outlet/src/main/java/entity/Venda.java
@@ -1,5 +1,38 @@
 package entity;
 
-public class Venda {
+import lombok.Data;
 
+import java.util.List;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+@Data
+@Entity
+@Table(name ="vendas")
+public class Venda {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String enderecoEntrega;
+	
+	@NotNull
+	private Double valorTotal;
+	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
+	private Cliente cliente;
+	
+	@ManyToOne
+	@JoinColumn(name = "funcionario_id")
+	private Funcionario funcionario;
+	
+	@ManyToMany
+	@JoinTable(
+			name = "venda_produto",
+			joinColumns = @JoinColumn(name = "venda_id"), 
+			inverseJoinColumns = @JoinColumn(name = "produto_id")
+			)
+	private List<Produto> produtos;
+	
 }
--- outlet/src/main/java/repository/ClienteRepository.java
+++ outlet/src/main/java/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package repository;
 
-public class ClienteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class ClienteRepository extends JpaRepository<Cliente, Long> {
 
 }
