7efa95091d0445a802651170f6ef0629b69f74b6
nicholasnln <nicholasnloiola@hotmail.com>
2024-08-25 02:19:46 -0300

    Adicionando e atualizando Cliente e Funcionario

--- outlet/src/main/java/entity/Cliente.java
+++ outlet/src/main/java/entity/Cliente.java
@@ -3,12 +3,40 @@ package entity;
 import lombok.Data;
 import java.util.List;
 import jakarta.persistence.Entity;
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotNull; 
+
+import org.springframework.data.annotation.Id;
 
 @Data
 @Entity
-@Table
+@Table(name = "clientes")
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Nome não pode ser nulo")
+    @Size(min = 2, max = 100, message = "Nome deve ter entre 2 e 100 caracteres")
+    private String nome;
+
+    @NotNull(message = "CPF não pode ser nulo")
+    @Pattern(regexp = "\\d{11}", message = "CPF deve ter 11 dígitos")
+    private String cpf;
+
+    @NotNull(message = "Idade não pode ser nula")
+    private Integer idade;
+
+    @NotNull(message = "Telefone não pode ser nulo")
+    @Pattern(regexp = "\\(\\d{2}\\) \\d{4,5}-\\d{4}", message = "Telefone deve estar no formato (XX) XXXXX-XXXX")
+    private String telefone;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Venda> vendas;
 }
--- outlet/src/main/java/entity/Funcionario.java
+++ outlet/src/main/java/entity/Funcionario.java
@@ -1,5 +1,31 @@
 package entity;
 
+import lombok.Data;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+@Data
+@Entity
+@Table(name = "funcionarios")
 public class Funcionario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "Nome não pode ser nulo")
+	@Size(min = 2, max = 100, message = "Nome deve ter de 2 a 100 caracteres")
+	private String nome;
+	
+	@NotNull(message = "Idade não pode ser nula")
+	@Size(min = 1, max = 2, message = "Idade tem que ter um numero valido")
+	private Integer idade;
+	
+	@NotNull(message = "CPF não pode ser nulo")
+	@Pattern(regexp = "\\{11}", message = "CPF deve conter 11 digitos")
+	private String cpf;
+	
+	@NotNull
+	@Column(unique = true)//faz com que a matricula seja uma chave unica no banco de dados
+	private String matricula;
 
 }
